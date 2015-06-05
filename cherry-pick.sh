ca82c2e667e65e0a06ed2b42d64cb0964251e064
Esteban Guzzo <estebanguzzo@gmail.com>
2015-06-05 16:39:08 -0300

    Update patches

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -5,7 +5,7 @@ then
   echo -e $CL_BLU"Cherrypicking android_build"$CL_RST
   cd build
   git fetch https://github.com/EpirexDevProjects/android_build cm-12.1
-  git cherry-pick ac76f901df73bc21fa106199e6f2fdf7b435b481
-  git cherry-pick 013498bbfcaaa694fe82742d9fe6650bd66491a6
+  git cherry-pick 653803acda79dc9eb52def64f51a0f95ec18ce1e
+  git cherry-pick 8b181a166944b88899bf3293b67b7801b4c58e16
   cd ..
 fi
