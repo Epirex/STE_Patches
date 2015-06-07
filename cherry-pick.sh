3a27a1fac81af383fab949757717e6c48942d7b9
Esteban Guzzo <estebanguzzo@gmail.com>
2015-06-07 20:11:42 -0300

    Add patch "Don't build OTAs"

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -7,5 +7,6 @@ then
   git fetch https://github.com/EpirexDevProjects/android_build cm-12.1
   git cherry-pick 653803acda79dc9eb52def64f51a0f95ec18ce1e
   git cherry-pick 8b181a166944b88899bf3293b67b7801b4c58e16
+  git cherry-pick 9ff0ee9a95657cc915a6e69a328b37cb99ca9d5a
   cd ..
 fi
