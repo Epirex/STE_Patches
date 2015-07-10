b8fac7abee2e20b0cf46598c91407442da070682
Esteban Guzzo <estebanguzzo@gmail.com>
2015-07-10 00:43:47 -0300

    Add some patches

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -6,7 +6,42 @@ then
   cd build
   git fetch https://github.com/EpirexDevProjects/android_build cm-12.1
   git cherry-pick 653803acda79dc9eb52def64f51a0f95ec18ce1e
-  git cherry-pick 8b181a166944b88899bf3293b67b7801b4c58e16
   git cherry-pick 9ff0ee9a95657cc915a6e69a328b37cb99ca9d5a
   cd ..
+fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_frameworks_av"$CL_RST
+  cd frameworks/av
+  git fetch https://github.com/EpirexDevProjects/android_frameworks_av cm-12.1
+  git cherry-pick 3f5e41de9e8a9f11640380ac3e32a1897dbccfc1
+  cd ../..
+fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_frameworks_native"$CL_RST
+  cd frameworks/native
+  git fetch https://github.com/EpirexDevProjects/android_frameworks_native cm-12.1
+  git cherry-pick ce62b9754d5fdcf0fd0587aade5eae1d2b5d32a7
+  cd ../..
+fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_system_core"$CL_RST
+  cd system/core
+  git fetch https://github.com/EpirexDevProjects/android_system_core cm-12.1
+  git cherry-pick 1764365eae1e3efe7b460d3937e4b0f1a71a19ec
+  cd ../..
+fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_frameworks_base"$CL_RST
+  cd frameworks/base
+  git fetch https://github.com/EpirexDevProjects/android_frameworks_base cm-12.1
+  git cherry-pick 83385561d5d5c031a26bfa263606264b1262a4b2
+  cd ../..
 fi
