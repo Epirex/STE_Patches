f104482052114c8fe4a63ed3f1ac711cce7f8735
Esteban Guzzo <estebanguzzo@gmail.com>
2015-07-12 03:13:22 -0300

    Update native patches
    For fix camera video bug

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -32,14 +32,16 @@ then
     git fetch https://github.com/cernekee/android_art monitor-stack-v1
     git cherry-pick fc2ac71d0d9e147c607bff9371fe2ef25d8470af
     cd ..
-    echo -e $CL_BLU"Cherrypicking for android_frameworks_av - STE Multimedia Fix [1/4]"$CL_RST
+    echo -e $CL_BLU"Cherrypicking for android_frameworks_av - Remove Mediatek changes"$CL_RST
     cd frameworks/av
     git fetch https://github.com/EpirexDevProjects/android_frameworks_av cm-11.0
-    git cherry-pick e725c9d3b2b321d879f5f2cb6f16c2c95adb8c8b
+    git cherry-pick d59aaafbe5068109d11a8a7322764194abc82617
+    echo -e $CL_BLU"Cherrypicking for android_frameworks_av - STE Multimedia Fix [1/4]"$CL_RST
+    git cherry-pick 10214a533db745bec7d6d2f44ffcdbf43a6f1948
     echo -e $CL_BLU"Cherrypicking for android_frameworks_av - STE-OMX: fix getSupportedProfileLevel"$CL_RST
-    git cherry-pick cd7cfee33fe7863f4d793ad2677bd95225c53267
+    git cherry-pick 128279fd3cf4e15aa4f77c124ae4fceda46f4f4a
     echo -e $CL_BLU"Cherrypicking for android_frameworks_av - STE-OMX: video streaming fix"$CL_RST
-    git cherry-pick ddbdd58dcb5a0227e009bf15b5e61180c7408e0b
+    git cherry-pick af44d0a6546b14dae0e5458f850f542373635fef
     cd ..
     echo -e $CL_BLU"Cherrypicking for android_frameworks_native - STE Multimedia Fix [2/4]"$CL_RST
     cd native
