70e78b5099f65978154dd10d9d17bd8425f5e301
Esteban Guzzo <estebanguzzo@gmail.com>
2015-07-25 10:02:05 -0300

    huge update

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -2,21 +2,10 @@
 
 if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
 then
-  echo -e $CL_BLU"Cherrypicking android_build"$CL_RST
-  cd build
-  git fetch https://github.com/EpirexDevProjects/android_build cm-12.1
-  git cherry-pick a22f17677852770220ef9fc74d0fb148a5beaf92
-  git cherry-pick 53037e3b014ac4e2638cd76c47d061e9d622cfaa
-  cd ..
-fi
-
-if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
-then
   echo -e $CL_BLU"Cherrypicking android_frameworks_av"$CL_RST
   cd frameworks/av
-  git fetch https://github.com/EpirexDevProjects/android_frameworks_av cm-12.1
-  git cherry-pick a938746abdaa7905f12f90dcf7e2d8e5cdf2d507
-  git cherry-pick af601ff31b3df2fea69793259bc3faa140c2d7dd
+  git fetch https://github.com/EpirexDevProjects/android_frameworks_av patch-1
+  git cherry-pick 1117a2993141a5b089e138213396ecc0d7e76f76
   cd ../..
 fi
 
@@ -44,5 +33,34 @@ then
   cd frameworks/base
   git fetch https://github.com/EpirexDevProjects/android_frameworks_base cm-12.1
   git cherry-pick 83385561d5d5c031a26bfa263606264b1262a4b2
+  git cherry-pick 1a644e14da7b41ea9f9d0314ee3156d3d8fa2ad5
   cd ../..
 fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_bionic"$CL_RST
+  cd bionic
+  git fetch https://github.com/EpirexDevProjects/android_bionic cm-12.1
+  git cherry-pick d1eb51d246db5fc086c218cdacf6f7cdd1cc38c7
+  cd ..
+fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_packages_services_Telecomm"$CL_RST
+  cd packages/services/Telecomm
+  git fetch https://github.com/EpirexDevProjects/android_packages_services_Telecomm cm-12.1
+  git cherry-pick c398004508b9e40897b2f8af9c1775edefa399d0
+  cd ../../..
+fi
+
+if [ "$REPO_BRANCH" = "cm-12.1" ] || [ "$DEVICE" = "janice" ]
+then
+  echo -e $CL_BLU"Cherrypicking android_frameworks_opt_telephony"$CL_RST
+  cd frameworks/opt/telephony
+  git fetch https://github.com/EpirexDevProjects/android_frameworks_opt_telephony cm-12.1
+  git cherry-pick 5eb846f60545d967617042b693e2d9af41c8d319
+  cd ../../..
+fi
+
